4ed8a9e51f6d8bff47e438bc91bacec8be3c3983
Restyled.io <commits@restyled.io>
2019-11-25 07:20:32 +0000

    Restyled by prettier

--- tests/kilometers-to-miles.test.js
+++ tests/kilometers-to-miles.test.js
@@ -2,8 +2,9 @@ import test from "ava";
 
 test("Kilometers to Miles test", t => {
   const input = 1;
-  const executedInput =
-      require("../recipes/kilometers-to-miles").execute(input);
+  const executedInput = require("../recipes/kilometers-to-miles").execute(
+    input
+  );
   const expectedResult = 0.621371;
   t.is(executedInput, expectedResult);
 });
