704a3d82cf46f5b65d98f581d349d3c9b5e0fb47
Restyled.io <commits@restyled.io>
2019-11-25 07:20:07 +0000

    Restyled by clang-format

--- tests/kilometers-to-miles.test.js
+++ tests/kilometers-to-miles.test.js
@@ -1,8 +1,9 @@
 import test from "ava";
 
 test("Kilometers to Miles test", t => {
-    const input = 1;
-    const executedInput = require("../recipes/kilometers-to-miles").execute(input);
-    const expectedResult = 0.621371;
-    t.is(executedInput, expectedResult);
+  const input = 1;
+  const executedInput =
+      require("../recipes/kilometers-to-miles").execute(input);
+  const expectedResult = 0.621371;
+  t.is(executedInput, expectedResult);
 });
